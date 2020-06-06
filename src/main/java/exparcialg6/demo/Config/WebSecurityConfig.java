92db04667df6dd025b060720173ec06e60c47630
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-06 18:14:06 -0500

    web sec conf

--- src/main/java/exparcialg6/demo/Config/WebSecurityConfig.java
+++ src/main/java/exparcialg6/demo/Config/WebSecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
-
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
