b2607932a2c29cfb88caba04e2e353b748ec9d58
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-06 16:31:00 -0500

    correccion Entity

--- src/main/java/exparcialg6/demo/entity/Pedido.java
+++ src/main/java/exparcialg6/demo/entity/Pedido.java
@@ -7,12 +7,38 @@ import java.util.Date;
 @Table(name="pedido")
 public class Pedido {
     @Id
-    @Column(name="idpedido")
+    @Column(nullable = false, name="idpedido")
+    private
     String idpedido;
-    @Column(nullable = false, name="Total")
+    @Column(nullable = false, name="total")
+    private
     Float total;
-    @Column(nullable = false, name="Fecha")
+    @Column(nullable = false, name="fecha")
+    private
     Date fecha;
 
 
+    public String getIdpedido() {
+        return idpedido;
+    }
+
+    public void setIdpedido(String idpedido) {
+        this.idpedido = idpedido;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
 }
--- src/main/java/exparcialg6/demo/entity/Usuario.java
+++ src/main/java/exparcialg6/demo/entity/Usuario.java
@@ -9,15 +9,71 @@ public class Usuario {
     @Id
     @Column(name="idusuario")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private
     Integer idusuario;
+    @Column(nullable = false, name="nombre")
+    private
     String nombre;
+    @Column(nullable = false, name="apellido")
+    private
     String apellido;
+    @Column(nullable = false, name="dni")
+    private
     Integer dni;
+    @Column(nullable = false, name="correo")
+    private
     String correo;
     @ManyToOne
     @JoinColumn(name = "idrol")
+    private
     Rol rol;
 
 
+    public Integer getIdusuario() {
+        return idusuario;
+    }
 
+    public void setIdusuario(Integer idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
 }
