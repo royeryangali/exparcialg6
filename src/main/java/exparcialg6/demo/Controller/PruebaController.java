e59f5d22435a875c802e8fe48bced5635398f8b1
Royer <r.yangali@pucp.pe>
2020-06-06 13:53:22 -0500

    pruebas iniciales Royer

---
+++ src/main/java/exparcialg6/demo/Controller/PruebaController.java
@@ -0,0 +1,15 @@
+package exparcialg6.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/prueba")
+public class PruebaController {
+    @GetMapping(value={"/",""})
+    public String prueba(){
+
+        return "login/loginForm";
+    }
+}
