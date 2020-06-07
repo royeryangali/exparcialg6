741c6f8f5810434bd4cc07023a60b39ca9150504
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-06 20:34:30 -0500

    xd

--- src/main/java/exparcialg6/demo/Controller/RegistradoController.java
+++ src/main/java/exparcialg6/demo/Controller/RegistradoController.java
@@ -1,5 +1,10 @@
 package exparcialg6.demo.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registrado")
 public class RegistradoController {
 
 }
