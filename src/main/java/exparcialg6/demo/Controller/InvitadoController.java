3ea77fd216aeb84f5e976c4efc2fe509a2a69087
Sebastian-RG <fullmetalliferous@gmail.com>
2020-06-08 00:45:20 -0500

    paginacion

--- src/main/java/exparcialg6/demo/Controller/InvitadoController.java
+++ src/main/java/exparcialg6/demo/Controller/InvitadoController.java
@@ -1,6 +1,7 @@
 package exparcialg6.demo.Controller;
 
 
+import exparcialg6.demo.entity.Producto;
 import exparcialg6.demo.entity.Usuario;
 import exparcialg6.demo.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/invitado")
 public class InvitadoController {
@@ -19,19 +23,27 @@ public class InvitadoController {
     ProductoRepository productoRepository;
 
 
+    @GetMapping(value = {"/listarProductos", "", "/"})  // LISTAR PRODUCTOS
+    public String listarProductos(Model model, RedirectAttributes attr,  @RequestParam(required = false) Integer pag) {
+        if(pag == null){
+            pag = 0;
+        }
+        List<Producto> xd = productoRepository.findAll();
+        int a = xd.size() / 7;
+dd
 
+        ArrayList<Producto> enviar = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            enviar.add(xd.get(i + pag*7));
+        }
 
-    @GetMapping(value={"/listarProductos","","/"})  // LISTAR PRODUCTOS
-    public String listarProductos( Model model, RedirectAttributes attr) {
-
-
-
-            model.addAttribute("listaProductos", productoRepository.findAll());
-            return "producto/listProduct";
+        model.addAttribute("listaProductos", enviar);
+        model.addAttribute("paginacion", a);
+        return "producto/listProduct";
     }
 
     @GetMapping("/listarVerMas") // VER UN SOLO PRODUCTO
-    public String VerMas( Model model,@RequestParam("id") int id ) {
+    public String VerMas(Model model, @RequestParam("id") int id) {
         model.addAttribute("producto", productoRepository.findById(id).get());
         return "producto/vermasProduct";
     }
@@ -43,11 +55,9 @@ public class InvitadoController {
     }
 
     @GetMapping("/recuperar") // MUESTRA LA PAGINA DE RECUPERAR CONTRA
-    public String recuperar( Model model) {
+    public String recuperar(Model model) {
         return "login/recoveryForm";
     }
-
-
 
 
 }
